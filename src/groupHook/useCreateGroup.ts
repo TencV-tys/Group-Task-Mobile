505e7f6b10d86e541e0be782cf58ea1051332e0f
Vincent <vincenttayros078@gmail.com>
2026-01-31 21:47:50 +0800

    added createGroupHook

---
+++ src/groupHook/useCreateGroup.ts
@@ -0,0 +1,68 @@
+
+
+import { useState } from "react";
+import { GroupService } from "../groupServices/GroupService";
+
+export function useCreateGroup(){
+    const [loading, setLoading]  = useState<boolean>(false);
+    const [message, setMessage ] = useState<string>('');
+    const [success, setSuccess ] = useState<boolean>(false);
+    
+    const createGroup = async(name:string, description?:string)=>{
+           setLoading(true);
+           setMessage('');
+           setSuccess(false);
+
+           try{
+            const result = await GroupService.createGroup(name,description);
+
+            if(result.success){
+                setSuccess(true);
+                setMessage(result.message || "Group created successfully" );
+                return{
+                    success:true,
+                    message:result.message,
+                    group:result.group,
+                    inviteCode:result.inviteCode
+                }
+
+            }else{
+                   setSuccess(false);
+        setMessage(`❌ ${result.message || 'Failed to create group'}`);
+        return {
+          success: false,
+          message: result.message
+        };
+            }
+
+           }catch(error:any){
+ console.error("useCreateGroup: Error:", error);
+      setSuccess(false);
+      setMessage(`❌ ${error.message || 'Network error'}`);
+      return {
+        success: false,
+        message: error.message || 'Network error'
+      };
+
+           }finally{
+            setLoading(false);
+           }
+ 
+
+    };
+
+    const reset = () =>{
+        setLoading(false);
+        setMessage('');
+        setSuccess(false);
+    }
+
+    return{
+        loading,
+        message,
+        success,
+        createGroup,
+        reset
+    }
+
+}

8e7e2c987ae9e68ca921ee9456e3e1a28f2502ae
Vincent <vincenttayros078@gmail.com>
2026-01-31 21:37:16 +0800

    added new function

---
+++ src/groupServices/GroupService.ts
@@ -0,0 +1,42 @@
+
+const API_URL = "http://10.219.65.2:5000/api/group";
+
+
+
+export class GroupService{
+         
+    static async createGroup(name:string, description?:string){
+            try{
+                
+                if(!name.trim() || !name ){
+                    return{
+                        success:false,
+                        message:"Group name is required"
+                    }
+                }
+
+                const response = await fetch(`${API_URL}/create`,{
+                    method:"POST",
+                    headers:{"Content-Type":"application/json"},
+                    body:JSON.stringify({name:name.trim(), description:description?.trim() || null}),
+                    credentials:'include'
+                });
+
+
+                const result = await response.json();
+            
+                return result;
+
+
+            }catch(e:any){
+                 return{
+                    success:false,
+                    message:"Failed to create group"
+                 }
+            }
+
+    }
+
+
+
+}
